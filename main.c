42f37d3e48c21c10c9fe3166b3276729b5ec79d6
gullabjamun <supermariocake@gmail.com>
2017-07-27 21:44:25 +0900

    make request, reply packet partially

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 
      int main(int argc, char *argv[])
      {
-	u_char send_packet[42]={0,};
+	u_char send_packet_arprequest[42]={0,};
 	u_char receive_packet[42];
 
 
@@ -26,9 +26,10 @@
 	u_char ipoff;
 	char ip_dst_str[16];
 	char ip_src_str[16];
-	struct arphdr arprequest;
-	struct arphdr arpreply;
-	struct sniff_ethernet *ethernet;
+	struct arphdr arprequest_arp;
+	struct arphdr arpreply_arp;
+	struct sniff_ethernet arprequest_eth;
+	struct sniff_ethernet arpreply_eth;
 	struct sniff_ip *ip;
 	struct sniff_tcp *tcp;
 	struct sniff_data *data;
@@ -38,6 +39,38 @@
 
 	u_char *sender_ip=argv[2];
 	u_char *target_ip=argv[3];
+
+	/* arp request to know target mac address*/
+////	arprequest_eth.ether_dhost={0xff,0xff,0xff,0xff,0xff,0xff};
+////	arprequest_eth.ether_shost={0x00,0x0c,0x29,0xe8,0xc7,0x22};
+	arprequest_eth.ether_type=htons(0x0806);
+
+	arprequest_arp.htype=htons(0x0001);
+	arprequest_arp.ptype=htons(0x0800);
+	arprequest_arp.hlen=htons(0x06);
+	arprequest_arp.plen=htons(0x04);
+	arprequest_arp.oper=htons(0x0001);
+////	arprequest_arp.sha={0x00,0x0c,0x29,0xe8,0xc7,0x22};
+//	arprequest_arp.spa=htons(0x0001);			//senderip-my ip
+////	arprequest_arp.tha={0,};
+//	arprequest_arp.tpa=htons(0x0001);			//targetip
+
+	/* attack arp reply */
+////	arpreply_eth.ether_dhost={target mac};
+////	arpreply_eth.ether_shost={0x00,0x0c,0x29,0xe8,0xc7,0x22}; 
+	arpreply_eth.ether_type=htons(0x0806);
+	inet_pton(AF_INET,sender_ip,(arpreply_arp).spa);
+	inet_pton(AF_INET,target_ip,(arpreply_arp).tpa);
+	arpreply_arp.htype=htons(0x0001);
+	arpreply_arp.ptype=htons(0x0800);
+	arpreply_arp.hlen=htons(0x06);
+	arpreply_arp.plen=htons(0x04);
+	arpreply_arp.oper=htons(0x0002);
+////	arpreply_arp.sha={0x00,0x0c,0x29,0xe8,0xc7,0x22};
+//	arpreply_arp.spa=htons(0x0001);				//sender ip - gateway
+////	arpreply_arp.tha={target mac};
+//	arpreply_arp.tpa=htons(0x0001);				//targetip
+	
 
         /* Define the device */
         dev = pcap_lookupdev(errbuf);
@@ -69,14 +102,13 @@
 
   	  while(1)
 	  {
-		inet_pton(AF_INET,sender_ip,(arprequest).spa);
-		inet_pton(AF_INET,target_ip,(arprequest).tpa);
+
 	      	printf("%s\n",sender_ip);
 		printf("%s\n",target_ip);
-	      	printf("%2x\n",*((arprequest).spa));
-		printf("%2x\n",*((arprequest).tpa));
+	      	printf("%2x\n",*((arpreply_arp).spa));
+		printf("%2x\n",*((arpreply_arp).tpa));
 
-		if(pcap_sendpacket(handle,send_packet,42)!=0)
+		if(pcap_sendpacket(handle,send_packet_arprequest,42)!=0)
 		{
 			printf("error\n");
 		}
